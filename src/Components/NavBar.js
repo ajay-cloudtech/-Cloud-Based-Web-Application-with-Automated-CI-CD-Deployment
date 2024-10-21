290ec977e3be9c6a2048519d8c5d01152e8d0572
Ajay <ajay.gemini90@gmail.com>
2024-10-21 22:57:51 +0100

    changing wise color to test pipeline

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -17,7 +17,7 @@ function NavBar({ toggleForm, buttonLabel }) {
 
     return (
         <nav id='navBar'>
-            <h1 id='heading1'>Grade<span style={{ color: 'red' }}>Wise</span></h1>
+            <h1 id='heading1'>Grade<span style={{ color: 'pink' }}>Wise</span></h1>
             <button id='addbtn' onClick={toggleForm}>{buttonLabel}</button>
             <div id = 'authBtn'>
                 {currentUser ? ( // Conditional rendering based on authentication state
